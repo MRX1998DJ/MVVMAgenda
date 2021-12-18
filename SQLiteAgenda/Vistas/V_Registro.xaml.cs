cf7a39744314c56d0270d192e840b04833629450
Bayron Muñoz <bayronjosue17@gmail.com>
2021-12-17 23:34:42 -0600

    Add project files.

---
+++ SQLiteAgenda/Vistas/V_Registro.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using SQLite;
+using SQLiteAgenda.Tablas;
+using SQLiteAgenda.Datos;
+
+namespace SQLiteAgenda.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class V_Registro : ContentPage
+    {
+        private SQLiteAsyncConnection conexion;
+        public V_Registro()
+        {
+            InitializeComponent();
+            conexion = DependencyService.Get<ISQLiteDB>().GetConnection();
+            btnGuardar.Clicked += BtnGuardar_Clicked;
+        }
+        private void BtnGuardar_Clicked(object sender, EventArgs e)
+        {
+            var DatosContacto = new T_Contacto
+            {
+                Nombre = txtNombre.Text,
+                Apellidos = txtApellidos.Text,
+                edad= txtedad.Text,
+                Direccion = txtdireccion.Text,
+                puesto = txtpuesto.Text
+            };
+            conexion.InsertAsync(DatosContacto);
+            limpiarFormulario();
+            DisplayAlert("Confirmación", "El contacto se registró correctamente", "OK");
+        }
+        private void limpiarFormulario()
+        {
+            txtNombre.Text = "";
+            txtApellidos.Text = "";
+            txtedad.Text = "";
+            txtdireccion.Text = "";
+            txtpuesto.Text = "";
+        }
+    }
+}
